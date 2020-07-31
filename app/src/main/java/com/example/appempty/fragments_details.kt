8c1f7502146d0f140fa4462387d443e5b611585a
Amaury <amaurycb@gmail.com>
2020-07-31 10:31:03 -0400

    Create fragments with list

--- app/src/main/java/com/example/appempty/fragments_details.kt
+++ app/src/main/java/com/example/appempty/fragments_details.kt
@@ -5,11 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
-import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.*
 
-
-import kotlinx.android.synthetic.main.activity_main.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,10 +17,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ListaFragment.newInstance] factory method to
+ * Use the [fragments_details.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListaFragment : Fragment() {
+class fragments_details : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,22 +33,7 @@ class ListaFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
 
-        return inflater.inflate(R.layout.fragment_fragments_details, container, false)
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<TextView>(R.id.text1).setOnClickListener {
-            findNavController().navigate(R.id.action_ListaFragment_to_fragments_details)
-
-
-        }
-    }
 
     companion object {
         /**
@@ -58,12 +42,12 @@ class ListaFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ListaFragment.
+         * @return A new instance of fragment fragments_details.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ListaFragment().apply {
+            fragments_details().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
