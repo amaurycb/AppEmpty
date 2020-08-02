97f3de9ffb5a9b767d4b52b457ac528eb2f64c0c
William Gutierrez <william.gutierreztor@rallyhealth.com>
2020-08-02 12:05:25 -0700

    Set up the Navigation Controller

--- app/src/main/java/com/example/appempty/MainActivity.kt
+++ app/src/main/java/com/example/appempty/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.appempty
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
 
